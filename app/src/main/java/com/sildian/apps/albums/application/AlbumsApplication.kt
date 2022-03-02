acb124c1dac8edeeb2b2eeef641b5b18bc338931
Nicolas MAYER-WEILER <sildian@MacBook-Air-de-Nicolas.local>
2022-03-02 13:17:21 +0100

    Turn the app to a Hilt Android app

---
+++ app/src/main/java/com/sildian/apps/albums/application/AlbumsApplication.kt
@@ -0,0 +1,11 @@
+package com.sildian.apps.albums.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/*************************************************************************************************
+ * Application component
+ ************************************************************************************************/
+
+@HiltAndroidApp
+class AlbumsApplication: Application()
